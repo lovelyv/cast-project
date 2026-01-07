4f685cb11151fdd7ec62321919ab9d7d3ee6d98c
Akash Agarwal <akash@Akashs-MacBook-Air.local>
2026-01-06 21:22:42 -0800

    Update vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
-  
-})
+export default defineConfig(({ command, mode }) => {
+  console.log("mode is:", mode); // production, development, github etc.
+
+  return {
+    base: mode === "github" ? "/cast-project/" : "/",
+    plugins: []
+  };
+});
+
 
 
