4d6eeb369be828d3e4328eeb06d03e36861039dd
Akash Agarwal <akash@Akashs-MacBook-Air.local>
2026-01-10 21:18:10 -0800

    Save all changes before gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  console.log("mode is:", mode); // production, development, github etc.
-
-const routerBasename = mode === "github" ? "/cast-project/" : "/";
-
-  return {
-   base: routerBasename,
-    define: {
-      "import.meta.env.VITE_ROUTER_BASENAME": JSON.stringify(routerBasename)
-    },
-    plugins: [react()]
-  };
-});
-
-
-
-
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/cast-project/' : '/',
+  plugins: [react()],
+}))
